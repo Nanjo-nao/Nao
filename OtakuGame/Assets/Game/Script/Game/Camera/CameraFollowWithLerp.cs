413f9ce382da6598a88e2af0792fec8e432c8218
Aieylisyh <Aieylisyh@users.noreply.github.com>
2022-07-20 12:40:31 +0800

    fast pass

--- OtakuGame/Assets/Game/Script/Game/Camera/CameraFollowWithLerp.cs
+++ OtakuGame/Assets/Game/Script/Game/Camera/CameraFollowWithLerp.cs
@@ -9,6 +9,8 @@ public class CameraFollowWithLerp : MonoBehaviour
     bool _endStartTween;
     public float duration = 2.5f;
     public float startOffsetRatio = 0.2f;
+    public Transform fastpass;
+
     void Start()
     {
         _offset = transform.position - player.position;
@@ -20,9 +22,26 @@ public class CameraFollowWithLerp : MonoBehaviour
         transform.DOMove(player.position + _offset, duration).SetEase(Ease.InOutQuad).OnComplete(
             () =>
             {
+                if (fastpass != null)
+                {
+                    move.startDest = null;
+                    player.gameObject.SetActive(false);
+                    player.position = fastpass.position;
+                    Invoke("ResumeFastPass", 0.5f);
+                    return;
+                }
+
                 move.enabled = true;
                 _endStartTween = true;
             });
+    }
+
+    void ResumeFastPass()
+    {
+        player.position = fastpass.position;
+        player.gameObject.SetActive(true);
+        move.enabled = true;
+        _endStartTween = true;
     }
 
     void Update()
